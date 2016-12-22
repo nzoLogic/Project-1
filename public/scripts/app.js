90cb8729940cdcda85b0d9c240f5e9d694a05445
ricarellano <rarellanob@gmail.com>
2016-12-22 12:53:20 -0800

    Ajax post & handlebar

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,5 +1,60 @@
 console.log('app.js online');
+var $momentsFeed;
 
 $(document).ready(function(){
   $('select').material_select();
+
+  $momentsFeed = $('#momentsFeed');
+
+  var source = $('#moments-template').html();
+    template = Handlebars.compile(source);
+
+    $.ajax({
+      method: 'GET',
+      url: '/api/moments',
+      success: handleSuccess,
+      error: handleError
+    });
+
+    $('#textarea1').on('submit', function(e) {
+      console.log('clicked')
+       e.preventDefault();
+       console.log('new moment serialized', $(this).serializeArray());
+       $.ajax({
+         method: 'POST',
+         url: '/api/moments',
+         data: $(this).serializeArray(),
+         success: newMomentSuccess,
+         error: momentError
+       });
+     });
+
+     function render () {
+
+      //  $momentsFeed.empty();
+
+
+       var momentsHtml = template({ moments: moments });
+
+
+       $momentsFeed.append(momentsHtml);
+     }
+
+     function newMomentSuccess(moments) {
+       $('#textarea1 input').val('');
+
+       json.forEach(moments);
+       render();
+     }
+
+     function newMomentError() {
+       console.log('new moment error!');
+     }
+
+
+
+
+
+
+
 });
