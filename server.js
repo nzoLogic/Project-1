7e2de707e4316823784653c71fa8565af34333fd
nzoLogic <nzoLogic@gmail.com>
2016-12-22 11:58:29 -0800

    required the controllers directory that house the route callback functions

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require('express'),
     app = express(),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    controllers = require('./controllers');
 
 //serve static files from public folder
 app.use(express.static('public'));
@@ -12,6 +13,11 @@ app.get('/', function(req, res) {
     });
 })
 
+//get path for retrieving all moments
+app.get('/api/moments', controllers.moments.allMoments);
+
+//post path for adding a moment
+app.post('/api/moments', controllers.moments.post);
 //server listen to env port or local 3000
 app.listen(process.env.port || 3000, function() {
     console.log('listening on port 3k')
